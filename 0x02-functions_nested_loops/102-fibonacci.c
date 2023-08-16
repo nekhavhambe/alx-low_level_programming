1af73ed394114aa3608831e803da575c79ff917a
nekhavhambe <78914827+nekhavhambe@users.noreply.github.com>
2023-08-16 22:02:24 +0200

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,23 +12,22 @@
 
 int main(void)
 {
+	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	float total_sum;
 
-	while (1)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-
-		if (sum > 4000000)
-			break;
-
-		if ((sum % 2) == 0)
-			total_sum += sum;
+		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
-	printf("%.0f\n", total_sum);
 
 	return (0);
 }
