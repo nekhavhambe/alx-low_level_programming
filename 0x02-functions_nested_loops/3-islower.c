6b32ac78d5847806d47c0e8bfef63c4570284ff0
nekhavhambe <78914827+nekhavhambe@users.noreply.github.com>
2023-08-16 21:36:47 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,7 @@
  * _islower - function to check if
  *           character is lowercase
  *
+ * @c: checks input of function
  *
  * Return: returns 1 if `c` is lowercase
  *         otherwise always 0 (Success)
