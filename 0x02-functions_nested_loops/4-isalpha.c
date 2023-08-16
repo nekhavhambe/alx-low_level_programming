bed5749a273c5210b37bf0c74462af0ed88f96ef
nekhavhambe <78914827+nekhavhambe@users.noreply.github.com>
2023-08-16 21:37:13 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,9 @@
 /**
  * _isalpha - checks if character is a letter
  *            both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
  * Return: 1 is c if true else 0
 */
 
